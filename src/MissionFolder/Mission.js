82b8a2eaef7a585d06478c3642d7f8da2eecbd0b
etefiok <diligentsamuel@gmail.com>
2024-04-03 11:47:23 +0100

    all new files

--- src/MissionFolder/Mission.js
+++ src/MissionFolder/Mission.js
@@ -12,21 +12,15 @@ function Mission() {
                 <h1><FontAwesomeIcon icon={faInfoCircle} /></h1>
                     <p>
                         <h2> ABOUT US</h2>[School Name] is a leading secondary school focused on academic excellence, character development, and community engagement.
-                        
-                         
-                        
-                    </p>
+              </p>
                         <button className=" Goodluckbutton" onClick={() => { window.location.href = "./About_Us_Page";}}>Read More</button>
             </div>
 
-            <div className="mission-statement">
-                <h1><FontAwesomeIcon icon={faInfoCircle} /></h1>
-                    <p>
-                        <h2> ABOUT US</h2>[School Name] is a leading secondary school focused on academic excellence, character development, and community engagement. 
-                        
-                    </p>
-                        <button className=" Goodluckbutton">Read More</button>
-            </div>
+                <span className="mission-statement">
+                    <img src={require("../images.jpg/Jamblogo.png")}/>
+                    <button className=" Goodluckbutton">CBT Center</button>
+                    
+                </span>
 
             <div className="mission-statement">
                 <h1><FontAwesomeIcon icon={faInfoCircle} /></h1>
