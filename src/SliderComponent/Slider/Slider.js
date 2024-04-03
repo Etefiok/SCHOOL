03c7c6f2f8a0904e9d0f1d48df4b035c396e804e
etefiok <diligentsamuel@gmail.com>
2024-04-03 09:56:41 +0100

    all new files

--- src/SliderComponent/Slider/Slider.js
+++ src/SliderComponent/Slider/Slider.js
@@ -1,5 +1,5 @@
-// import React from "react";
-// import "./Slider.css";
+import React from "react";
+import "./Slider.css";
 // import { sliderData } from "./Slider-data";
 // import {useState, useEffect} from "react";
 // import { AiOutlineArrowLeft, AiOutlineArrowRight } from 
@@ -7,8 +7,10 @@
 // import School_Calendar_on_front from "../../School_Calendar_on_front";
 // import Mission from "../../MissionFolder/Mission";
 // import { useNavigate } from 'react-router-dom';
+import Carousel from 'react-bootstrap/Carousel';
 
-//   //for side bar navigation 
+
+  //for side bar navigation 
 // import Button from 'react-bootstrap/Button';
 // import Offcanvas from 'react-bootstrap/Offcanvas';
 // import Dropdown from 'react-bootstrap/Dropdown';
@@ -17,14 +19,14 @@
 
 
 
-// const Slider = () => {
+const Slider = () => {
 
 
-//     const [currentSlide, setCurrentSlide] = useState(0);
-//     const slidelength = sliderData.length;
-// // SlideLength = 1 2 3
+    // const [currentSlide, setCurrentSlide] = useState(0);
+    // const slidelength = sliderData.length;
+// SlideLength = 1 2 3
 
-//   //for side bar navigation 
+  //for side bar navigation 
 // const [show, setShow] = useState(false);
 // const [value, setValue] = useState('');
 
@@ -112,98 +114,70 @@
 
 
 
-//     const handleButtonClickIndex0 = () => {
-//         window.location.href = "Library";
-//     };
+    // const handleButtonClickIndex0 = () => {
+    //     window.location.href = "Library";
+    // };
     
-//     const handleButtonClickIndex1 = () => {
-//         window.location.href = ("https://www.google.com");
-//     };
+    // const handleButtonClickIndex1 = () => {
+    //     window.location.href = ("https://www.google.com");
+    // };
 
 
-//     return (
-//         <div className="slider">
-//             <AiOutlineArrowLeft className='arrow prev' onClick={prevslide}/>
-//             <AiOutlineArrowRight className='arrow next' onClick={nextslide} />
+    return (
+        <div className="slider">
 
-//             {sliderData.map((slide, index) => {
-//                 return (
-//                     <div className={index === currentSlide ? "slide current" : "slide"} key={index}>
-//                         {index === currentSlide && (
-//                             <>
-//                                 <img src={slide.image} alt="" />
-                                
-                               
-//                                 <div className="content">
-//                                     <h4>{slide.heading}</h4>
-//                                     <hr />
-//                                     {index === 0 ? (
-//                                         <button className="btn btn-primary" onClick={handleButtonClickIndex0}>Get Started1</button>
-//                                     ) : index === 1 ? (
-//                                         <button className="btn btn-primary" onClick={handleButtonClickIndex1}>Get Started2</button>
-//                                     ) : index === 2 ? (
-//                                         <button className="btn btn-primary" onClick={() => { window.location.href = "./Library"; }}>Get Started3</button>
-//                                     ) : null}
-
-//                                 </div>
-//                             </>
-//                         )}
-
-//                     </div>
-//                 )
-//             })}
-//         </div>
-//     )
-// }
-
-// export default Slider
-
-
-
-
-import Carousel from 'react-bootstrap/Carousel';
-
-
-function Slider() {
-  return (
-    <Carousel data-bs-theme="dark">
+<Carousel data-bs-theme="dark">
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          // className="d-block w-100"
           src="https://upload.wikimedia.org/wikipedia/commons/8/82/2009-0617-Ontonagon-school.jpg"
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <Carousel.Caption className="Write-Up-Heading">
+          <h5>
+            <span>First</span> slide label <br></br>
+            <button>Get Started</button>
+          </h5>
+          {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          // className="d-block w-100"
           src="https://th.bing.com/th/id/R.2e4f9a711d7f79be125e21701d321875?rik=B2hg%2baf53yWKSg&riu=http%3a%2f%2fupload.wikimedia.org%2fwikipedia%2fcommons%2f8%2f8e%2fStaples_High_School%2c_Westport%2c_CT.jpg&ehk=DQxa8SaDEqX58tggI%2fnzADXWPLs%2f3cLjh6B9AUOcLl8%3d&risl=1&pid=ImgRaw&r=0"
           alt="Second slide"
         />
-        <Carousel.Caption >
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <Carousel.Caption className="Write-Up-Heading">
+          <h5>
+            <span>Second </span>slide label<br></br>
+            <button>Get Started</button>
+          </h5>
+          {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="d-block w-100"
+          // className="d-block w-100"
           src="https://upload.wikimedia.org/wikipedia/commons/3/39/Allatoona_High_School%2C_Cobb_County%2C_Georgia.JPG"
           alt="Third slide"
         />
-        <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>
+        <Carousel.Caption className="Write-Up-Heading">
+          <h5>
+            <span>Third </span> slide label<br></br>
+            <button>Get Started</button>
+          </h5>
+          {/* <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          </p> */}
+          
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-  );
+        </div>
+    )
 }
 
-export default Slider;
+export default Slider
+
+
+
