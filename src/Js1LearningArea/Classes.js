9e5b4d9751b87c3f02083422f63766c70dcf5716
etefiok <diligentsamuel@gmail.com>
2024-04-04 19:05:41 +0100

    all new files

--- src/Js1LearningArea/Classes.js
+++ src/Js1LearningArea/Classes.js
@@ -10,23 +10,26 @@ function Classes() {
 
 
     return(
-        <div className='choose'>
-                <NavBar_Student />
-            <div className='choose_class_container'>
-                <div className='choose_class'>
-                    <p>
-                        CHOOSE CLASS
-                    </p>
+        <div>
+            <NavBar_Student />
+                <div className='Space'></div>
+                <div className='choose'>
+                    <div className='choose_class_container'>
+                        <div className='choose_class'>
+                            <p>
+                                CHOOSE CLASS
+                            </p>
+                        </div>
+                            <div className='Classes'>
+                                <button onClick={() => { window.location.href = "./Js1SubjectForLearning"; }}>JS1</button>
+                                <button>JS2</button>
+                                <button>JS3</button>
+                                <button>SS1</button>
+                                <button>SS2</button>
+                                <button>SS3</button>
+                            </div>
+                    </div>
                 </div>
-                <div className='Classes'>
-                <button onClick={() => { window.location.href = "./Js1SubjectForLearning"; }}>JS1</button>
-                    <button>JS2</button>
-                    <button>JS3</button>
-                    <button>SS1</button>
-                    <button>SS2</button>
-                    <button>SS3</button>
-                </div>
-            </div>
         </div>
         )
         
--- src/Js1LearningArea/ClassesForExams.js
+++ src/Js1LearningArea/ClassesForExams.js
@@ -17,27 +17,25 @@ function ClassesForExams() {
 
     return(
     <div>
-            <NavBar_Student />
-            <div className='Space'>
-
-            </div>
-            <div className='choose'>
-        <div className='choose_class_container'>
-            <div className='choose_class'>
-                <p>
-                    CHOOSE CLASS
-                </p>
-            </div>
-            <div className='Classes'>
-            <button onClick={() => { window.location.href = "./Subjects_For_Exams"; }}>JS1</button>
-                <button>JS2</button>
-                <button>JS3</button>
-                <button>SS1</button>
-                <button>SS2</button>
-                <button>SS3</button>
-            </div>
-        </div>
-    </div>
+        <NavBar_Student />
+            <div className='Space'></div>
+                <div className='choose'>
+                    <div className='choose_class_container'>
+                        <div className='choose_class'>
+                            <p>
+                                CHOOSE CLASS
+                            </p>
+                        </div>
+                            <div className='Classes'>
+                                <button onClick={() => { window.location.href = "./Subjects_For_Exams"; }}>JS1</button>
+                                <button>JS2</button>
+                                <button>JS3</button>
+                                <button>SS1</button>
+                                <button>SS2</button>
+                                <button>SS3</button>
+                            </div>
+                    </div>
+                </div>
     </div>
     )
     
